3c72c80ef57c4921e47ed4ebc70a68f86876abdd
Mieszko Borodziuk <mieszko.borodziuk@gmail.com>
2020-09-30 00:18:09 +0200

    useEffect

--- Hooki/wprowadzenie/src/App.js
+++ Hooki/wprowadzenie/src/App.js
@@ -1,18 +1,49 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [isActive, setIsActive] = useState(true);
 
   const handler = () => setCounter(counter + 1);
+  const toggleVisibilityCounter = () => setIsActive(precValue => !precValue);
+
+  const counterComponent = isActive
+    ? <Counter rerenderCounter={counter} /> : null;
+
+  // useEffect(() => {
+  //    alert('hello')
+  // }, [])
 
   return (
     <div>
-      <h1 onClick={handler}>Hello</h1>
-      <p>{counter}</p>
-      <p></p>
+
+      <button onClick={toggleVisibilityCounter}>Pokaz/ukryj</button>
+      <button onClick={handler}> rerender</button>
+      {counterComponent}
     </div>
   );
+}
+
+const Counter = ({ rerenderCounter }) => {
+  const [counter, setCounter] = useState(0);
+
+  const handleMauseMove = event => setCounter(event.clientX);
+
+  useEffect(() => {
+    window.addEventListener('mousemove', handleMauseMove);
+
+    return () => {
+      alert('odmontowane')
+      window.removeEventListener('mousemove', handleMauseMove);
+    }
+  }, [rerenderCounter]);
+  return (
+    <div>
+      <p>{counter}</p>
+      <p>{rerenderCounter}</p>
+    </div>
+  )
 }
 
 export default App;
